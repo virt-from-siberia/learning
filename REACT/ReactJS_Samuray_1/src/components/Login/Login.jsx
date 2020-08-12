8b24f669e9bdec873948764361069fa6901c7c4f
Aleksey Elchin <alonso85@mail.ru>
2020-08-13 00:49:19 +0900

    edit

--- REACT/ReactJS_Samuray_1/src/components/Login/Login.jsx
+++ REACT/ReactJS_Samuray_1/src/components/Login/Login.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 import { Input } from "../common/FormsControls/FormsControls";
 import { required } from "../../utils/validators/validators";
+import { connect } from "react-redux";
+import login from "../../redux/auth-reducer";
+import { Redirect } from "react-router-dom";
 
 const LoginForm = (props) => {
   return (
     <form onSubmit={props.handleSubmit}>
       <div>
         <Field
-          placeholder={"Login"}
-          name={"login"}
+          placeholder={"Email"}
+          name={"email"}
           validate={[required]}
           component={Input}
         />
@@ -37,8 +40,12 @@ const LoginReduxForm = reduxForm({ form: "login" })(LoginForm);
 
 const Login = (props) => {
   const onSubmit = (formData) => {
-    console.log(formData);
+    props.login(formData.email, formData.password, formData.rememberMe);
   };
+
+  if (props.isAuth) {
+    return <Redirect to='/profile' />;
+  }
 
   return (
     <div>
@@ -48,4 +55,10 @@ const Login = (props) => {
   );
 };
 
-export default Login;
+const mapStateToProps = (state) => ({
+  isAuth: state.auth.isAuth,
+});
+
+export default connect(mapStateToProps, {
+  login,
+})(Login);
--- REACT/ReactJS_Samuray_1/src/redux/auth-reducer.js
+++ REACT/ReactJS_Samuray_1/src/redux/auth-reducer.js
@@ -1,40 +1,55 @@
-import {authAPI} from "../api/api";
+import { authAPI } from "../api/api";
 
-const SET_USER_DATA = 'SET_USER_DATA';
-
+const SET_USER_DATA = "SET_USER_DATA";
 
 let initialState = {
-    userId: null,
-    email: null,
-    login: null,
-    isAuth: false
+  userId: null,
+  email: null,
+  login: null,
+  isAuth: false,
 };
 
 const authReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case SET_USER_DATA:
+  switch (action.type) {
+    case SET_USER_DATA:
+      return {
+        ...state,
+        ...action.payload,
+      };
 
-            return {
-                ...state,
-                ...action.data,
-                isAuth: true
-            }
+    default:
+      return state;
+  }
+};
 
-        default:
-            return state;
-    }
-}
+export const setAuthUserData = (userId, email, login, isAuth) => ({
+  type: SET_USER_DATA,
+  payload: { userId, email, login, isAuth },
+});
 
-
-export const setAuthUserData = (userId, email, login) => ({type: SET_USER_DATA, data: {userId, email, login}  })
 export const getAuthUserData = () => (dispatch) => {
-    authAPI.me()
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                let {id, login, email} = response.data.data;
-                dispatch(setAuthUserData(id, email, login));
-            }
-        });
-}
+  authAPI.me().then((response) => {
+    if (response.data.resultCode === 0) {
+      let { id, login, email } = response.data.data;
+      dispatch(setAuthUserData(id, email, login, true));
+    }
+  });
+};
+
+export const login = (email, password, rememberMe) => (dispatch) => {
+  authAPI.login(email, password, rememberMe).then((response) => {
+    if (response.data.resultCode === 0) {
+      dispatch(getAuthUserData());
+    }
+  });
+};
+
+export const logout = () => (dispatch) => {
+  authAPI.logout().then((response) => {
+    if (response.data.resultCode === 0) {
+      dispatch(setAuthUserData(null, null, null, false));
+    }
+  });
+};
 
 export default authReducer;
