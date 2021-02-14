a63b035c534d183a9d205d6fc6561e8ff164d02e
alons <alonso85@mail.ru>
2021-02-14 08:50:42 +0300

    tetris

--- C#/[OTUS] С#/lesson3/Tetris/Tetris/Program.cs
+++ C#/[OTUS] С#/lesson3/Tetris/Tetris/Program.cs
@@ -6,27 +6,7 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.SetWindowSize(40, 30);
-            Console.SetBufferSize(40, 30);
-
-            int x1 = 2;
-            int y1 = 3; 
-            char c1 = '*';
-
-            Draw(x1, y1, c1);
-
-            int x2 = 2;
-            int y2 = 3;
-            char c2 = '*';
-
-            Draw(x2, y2, c2);
-
-            Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char c)
-        {
-            Console.SetCursorPosition(x, y);
+            Console.WriteLine("Hello World!");
         }
     }
 }
