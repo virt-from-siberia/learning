8d754d68d9f14484813350dd68b61c9f7229e87b
alons <alonso85@mail.ru>
2021-03-08 17:06:18 +0900

    next js

---
+++ NEXT_JS/nextjs-course/pages/index.js
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>This is Home Page</h1>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ NEXT_JS/nextjs-course/pages/portfolio/[projectid].js
@@ -0,0 +1,9 @@
+function PortfolioProjectPage() {
+  return (
+    <div>
+      <h1>This is PortfolioProjectPage</h1>
+    </div>
+  );
+}
+
+export default PortfolioProjectPage;
