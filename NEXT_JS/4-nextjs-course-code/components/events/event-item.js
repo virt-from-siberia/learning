c883a2afe489689b79997aa1a09aac3012c1237b
Aelchin <AELCHIN@gitlab.locko-domain.ru>
2021-03-13 00:17:40 +0900

    next js new

--- NEXT_JS/4-nextjs-course-code/components/events/event-item.js
+++ NEXT_JS/4-nextjs-course-code/components/events/event-item.js
@@ -1,5 +1,8 @@
 import classes from "./event-item.module.css";
 import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -19,14 +22,21 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
         </div>
         <div className={classes.address}>
+          <AddressIcon />
           <address>{formattedAddress}</address>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore event</Button>
+          <Button link={exploreLink}>
+            <span> Explore event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
