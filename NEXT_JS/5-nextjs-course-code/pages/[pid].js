46d97fbd7e0428eb663539fd06d02580f73e4e2f
alons <alonso85@mail.ru>
2021-03-16 23:00:37 +0900

    note

---
+++ NEXT_JS/5-nextjs-course-code/pages/[pid].js
@@ -0,0 +1,49 @@
+import path from "path";
+import fs from "fs/promises";
+function ProductDetailPage(props) {
+  const { loadedProduct } = props;
+
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </>
+  );
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const productId = params.pid;
+
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  const product = data.products.find((product) => product.id === productId);
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          pid: "p1",
+        },
+      },
+    ],
+    fallback: "blocking",
+  };
+}
+
+export default ProductDetailPage;
--- NEXT_JS/5-nextjs-course-code/pages/index.js
+++ NEXT_JS/5-nextjs-course-code/pages/index.js
@@ -1,39 +1,53 @@
-import dynamic from "next/dynamic";
-import {
-  WithUserAgentProps,
-  useUserAgent,
-  withUserAgent,
-} from "next-useragent";
+import path from "path";
+import fs from "fs/promises";
+import Link from "next/link";
 
 function HomePage(props) {
-  // console.log(props);
   const { products } = props;
 
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}>
+          <Link href={`${product.id}`}>{product.title}</Link>
+        </li>
       ))}
     </ul>
   );
 }
 
-// export async function getStaticProps() {
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
+}
+
+// export function getServerSideProps(context) {
+//   console.log(context.req.headers["user-agent"]);
+
 //   return {
 //     props: {
 //       products: [{ id: "p1", title: "Product1" }],
 //     },
 //   };
 // }
-
-export function getServerSideProps(context) {
-  console.log(context.req.headers["user-agent"]);
-
-  return {
-    props: {
-      products: [{ id: "p1", title: "Product1" }],
-    },
-  };
-}
 
 export default HomePage;
