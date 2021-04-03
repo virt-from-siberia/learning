fc0b65ad58e8b9fe9a348a8ef9b27a0f26f369e0
Aelchin <AELCHIN@gitlab.locko-domain.ru>
2021-04-04 00:00:09 +0900

    PC

---
+++ NEXT_JS/7-nextjs-course-code-05-prj-data-fetching/pages/events/index.js
@@ -0,0 +1,42 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+import { getAllEvents } from "../../helpers/api-util";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+
+function AllEventsPage(props) {
+  const router = useRouter();
+  const { events } = props;
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+
+  return (
+    <Fragment>
+      <Head>
+        <title>All events</title>
+        <meta name="description" content="All events and other events" />
+      </Head>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
+}
+
+export default AllEventsPage;
---
+++ NEXT_JS/7-nextjs-course-code-05-prj-data-fetching/pages/index.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import { getFeaturedEvents } from "../helpers/api-util";
+import EventList from "../components/events/event-list";
+
+function HomePage(props) {
+  return (
+    <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name="description" content="Find a lot of great events" />
+        <script></script>
+      </Head>
+      <EventList items={props.events} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
+}
+
+export default HomePage;
