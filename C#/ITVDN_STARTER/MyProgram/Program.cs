423e53b63b9428a3a8ec19fd3ff91318260fbcca
alons <alonso85@mail.ru>
2021-04-08 15:13:43 +0900

    notebook

--- C#/ITVDN_STARTER/MyProgram/Program.cs
+++ C#/ITVDN_STARTER/MyProgram/Program.cs
@@ -10,10 +10,25 @@ namespace MyProgram
     {
         static void Main(string[] args)
         {       
+
+            Int32 fullName = 1;
+            int allias = 2;
+            Console.WriteLine(fullName);
+            Console.WriteLine(allias);
+            Console.ReadKey();
             
+            int size = sizeof(decimal);
+            Console.WriteLine(size);
+            Console.ReadKey();
 
             float t = 28;
             Console.WriteLine(t);
+            Console.ReadKey();
+
+            int[] ageList = {1, 2 , 3};
+            int age = ageList[3];
+
+            Console.WriteLine(age);
             Console.ReadKey();
 
 
