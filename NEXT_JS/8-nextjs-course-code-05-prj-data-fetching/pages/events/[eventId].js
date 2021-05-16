2601199a231f562b0673287cafd67bfa30c088dd
alons <alonso85@mail.ru>
2021-05-16 21:27:37 +0900

    bolnitsa

---
+++ NEXT_JS/8-nextjs-course-code-05-prj-data-fetching/pages/events/[eventId].js
@@ -0,0 +1,66 @@
+import Head from "next/head";
+import { Fragment } from "react";
+
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
+
+function EventDetailPage(props) {
+  const event = props.selectedEvent;
+  const { title, date, location, image, description } = event;
+
+  if (!event) {
+    return (
+      <div className="center">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <Fragment>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Head>
+      <EventSummary title={title} />
+      <EventLogistics
+        date={date}
+        address={location}
+        image={image}
+        imageAlt={title}
+      />
+      <EventContent>
+        <p>{description}</p>
+      </EventContent>
+    </Fragment>
+  );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+    revalidate: 30,
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getFeaturedEvents();
+
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+
+  return {
+    paths: paths,
+    fallback: "blocking",
+  };
+}
+
+export default EventDetailPage;
---
+++ NEXT_JS/8-nextjs-course-code-05-prj-data-fetching/pages/index.js
@@ -0,0 +1,30 @@
+import Head from "next/head";
+
+import { getFeaturedEvents } from "../helpers/api-util";
+import EventList from "../components/events/event-list";
+
+function HomePage(props) {
+  return (
+    <div>
+      <Head>
+        <title>Next js Events</title>
+        <meta name="description" content="find a lot of things" />
+      </Head>
+
+      <EventList items={props.events} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
+}
+
+export default HomePage;
