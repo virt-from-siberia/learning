d9f9ac82e832ce795d37e4a99f91ca68a0af681f
alons <alonso85@mail.ru>
2021-06-15 04:57:19 +0900

    notebook

---
+++ NEXT_JS_RUS/test.ts
@@ -0,0 +1,19 @@
+let a: number = 4;
+const b: string = "sada";
+const c = true;
+
+let d: string[] = ["asdas, 322 ,'sad"];
+
+let e: any = 3;
+
+function test(a: string): string | string {
+  return "";
+}
+
+const test2 = (a: number): number => {
+  return a * 2;
+};
+
+d = d.map((x: string) => x.toLowerCase());
+
+function countCoordinate() {}
