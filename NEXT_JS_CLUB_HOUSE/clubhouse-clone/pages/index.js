08e68a75e9df7088fde0edade987f8c5fa271054
Aelchin <AELCHIN@gitlab.locko-domain.ru>
2021-06-17 18:30:25 +0900

    next js cone

--- NEXT_JS_CLUB_HOUSE/clubhouse-clone/pages/index.js
+++ NEXT_JS_CLUB_HOUSE/clubhouse-clone/pages/index.js
@@ -1,9 +1,18 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Link href="/room">Открыть</Link>
+      <Head>
+        <meta name="viewport" content="width=device-width" />
+        <title>This is a first page</title>
+      </Head>
+      <Link href="/room">
+        <a>
+          <h1>Открыть</h1>
+        </a>
+      </Link>
     </div>
   );
 }
