990d19cebcf360c35292a54713623e6647d85974
Aelchin <AELCHIN@gitlab.locko-domain.ru>
2021-06-22 03:51:32 +0900

    next js rus

--- NEXT_JS_RUS/project/top-app/layout/Sidebar/Sidebar.tsx
+++ NEXT_JS_RUS/project/top-app/layout/Sidebar/Sidebar.tsx
@@ -1,7 +1,13 @@
-import { SidebarProps } from "./Sidebar.props";
 import cn from "classnames";
+
+import { SidebarProps } from "./Sidebar.props";
+import { Menu } from "../Menu/Menu";
 import styles from "./Sidebar.module.css";
 
 export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
-  return <div {...props}>Sidebar</div>;
+  return (
+    <div {...props}>
+      <Menu />
+    </div>
+  );
 };
