6b435e761fe119f1885032aea28ecb859f817e76
Aleksey Elchin <alonso85@mail.ru>
2021-07-10 08:07:46 +0900

    gmail clone

---
+++ REACT/Sonny_Sangha_GMEAIL_Clone/gmail-clon/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header } from './Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1> Lets build gmail</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT/Sonny_Sangha_GMEAIL_Clone/gmail-clon/src/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import AppsIcon from '@material-ui/icons/Apps';
+
+import { IconButton } from '@material-ui/core';
+
+import './Header.css';
+
+export function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img src="https://cdn.worldvectorlogo.com/logos/gmail-icon-3.svg" alt="" />
+      </div>
+      <div className="header__middle">
+        <SearchIcon />
+        <input placeholder="Search mail" type="text" />
+        <ArrowDropDownIcon className="header__inputCaret" />
+      </div>
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
