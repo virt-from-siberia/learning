887c63ffc2ecc90eda2191889ce01aed2b646b3c
Aleksey Elchin <alonso85@mail.ru>
2021-11-26 15:54:08 +0900

    angular

--- ANGULAR/TBARTISHINOV/1-first/my-app/src/app/app.module.ts
+++ ANGULAR/TBARTISHINOV/1-first/my-app/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './views/categories/categories.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CategoriesComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
